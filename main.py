e2798de0c71e78391ed900cc9f48bf0114b47795
Luca G <luca.p.giannotti@gmail.com>
2022-10-31 09:44:14 -0500

    Added tower display and shell for tower placing Added new image files from .zip

--- main.py
+++ main.py
@@ -8,25 +8,28 @@ clock = pygame.time.Clock()
 
 #   Image Initalization
 map = pygame.image.load("Images\\map.png")
+tower1 = pygame.image.load("Images\\T_Arrow01.png")
 #   Initalize Variables
 screen = pygame.display.set_mode((1000,600))
-money = 1000
+mayra = 1000
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
-money_text = my_font.render(f'${money}', False, "red")
+money_text = my_font.render(f'${mayra}', False, "red")
+towers = []
 
 
 
 
 #   Classes
 class Tower():
-    def __init__(self, image, type):
+    def __init__(self, image, type, pos):
         self.image = image
         self.type = type
+        self.pos = pos
+
         
         self.range = 0
         self.damage = 0
         self.level = 1
-        self.pos = (500,300)
     
     def identifier(self, type):
         if type == None:
@@ -34,9 +37,16 @@ class Tower():
             self.damage = 0
 
 #   Functions
-# Count and Display All Towers
-def tower_placer():
-    pass
+# Create towers and place them
+def tower_placer(type):
+    if type == "arrow":
+        # place tower based on mouse click OR arrow movement
+        towers.append(Tower(tower1, type, (500,300)))        
+
+# Display towers (Always running)
+def tower_display():
+    for tower in towers:
+        screen.blit(tower.image, tower.pos)
 
 # Spawn enemies
 def enemy_spawner():
@@ -56,12 +66,17 @@ def enemy_mov():
 
 
 screen.blit(map,(0,0))
+screen.blit(tower1,(200,200))
 #   Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                tower_placer("arrow")
+    tower_display()
     screen.blit(money_text,(0,0))
-    pygame.display.flip()
     clock.tick(60)
+    pygame.display.flip()
