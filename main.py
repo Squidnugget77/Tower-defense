e71ce9e6390be4bf8328e5ec6e7fe6c0ef299c2d
Luca G <luca.p.giannotti@gmail.com>
2022-11-02 17:26:20 -0500

    Added tower placing and phantom tower functionality when placing

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 #   Import & Init
-import pygame, sys, random
+import pygame as alyssa
+import sys, random
 from pygame.locals import *
 
-pygame.init()
-pygame.display.set_caption("Tower Defense")
-clock = pygame.time.Clock()
+alyssa.init()
+alyssa.display.set_caption("Tower Defense")
+clock = alyssa.time.Clock()
 
 #   Image Initalization
-map = pygame.image.load("Images\\map.png")
-tower1 = pygame.image.load("Images\\T_Arrow01.png")
+tessa = alyssa.image.load("Images\\map.png")
+tower1 = alyssa.image.load("Images\\T_Arrow01.png")
 #   Initalize Variables
-screen = pygame.display.set_mode((1000,600))
+screen = alyssa.display.set_mode((1000,600))
 mayra = 1000
-my_font = pygame.font.SysFont('Comic Sans MS', 30)
+my_font = alyssa.font.SysFont('Comic Sans MS', 30)
 money_text = my_font.render(f'${mayra}', False, "red")
 towers = []
+phantom_towers = []
+key_press = 0
 
 
 
@@ -36,17 +39,36 @@ class Tower():
             self.range = 10
             self.damage = 0
 
+class Enemy():
+    pass
+
 #   Functions
 # Create towers and place them
 def tower_placer(type):
-    if type == "arrow":
-        # place tower based on mouse click OR arrow movement
-        towers.append(Tower(tower1, type, (500,300)))        
+    global key_press, phantom_towers, mayra, money_text
+    x, y = alyssa.mouse.get_pos()
+    if type == "magic":
+        if key_press == 0:
+            phantom_towers.append(Tower(tower1, type, (x,y)))
+            # place tower based on mouse click OR arrow movement
+        elif key_press == 1 and mayra >= 200:
+            towers.append(Tower(tower1, type, (x,y)))
+            key_press =- 1
+            phantom_towers = []
+            mayra -= 200
+            money_text = my_font.render(f'${mayra}', False, "red")
+        else:
+            print(f'You need $200 for that purchase! ${200-mayra} more!')
+
 
 # Display towers (Always running)
 def tower_display():
+    x, y = alyssa.mouse.get_pos()
     for tower in towers:
         screen.blit(tower.image, tower.pos)
+    for tower in phantom_towers:
+        screen.blit(tower.image,(x,y))
+    
 
 # Spawn enemies
 def enemy_spawner():
@@ -64,19 +86,22 @@ def start_level():
 def enemy_mov():
     pass
 
-
-screen.blit(map,(0,0))
-screen.blit(tower1,(200,200))
 #   Game Loop
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for event in alyssa.event.get():
+        if event.type == alyssa.QUIT:
+            alyssa.quit()
             sys.exit
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                tower_placer("arrow")
+        if event.type == alyssa.KEYDOWN:
+            if event.key == alyssa.K_1:
+                tower_placer("magic")
+                key_press += 1
+            if event.key == alyssa.K_ESCAPE:
+                key_press = 0
+                phantom_towers = []
+
+    screen.blit(tessa,(0,0))
     tower_display()
     screen.blit(money_text,(0,0))
+    alyssa.display.flip()
     clock.tick(60)
-    pygame.display.flip()
